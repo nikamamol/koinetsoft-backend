4838a4f21ebec707a58a20313fb99d5c283d0240
amol patil <amolspatil018@gmail.com>
2024-10-24 09:51:51 +0530

    add campaign changes and all

--- src/model/CreateTemplate.js
+++ src/model/CreateTemplate.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const templateSchema = new mongoose.Schema({
-    template_title: { type: String, required: true },
-    logo: { type: String, required: true },
-    banner: { type: String, required: true },
-    form_type: { type: String, required: true },
-    form_link: { type: String, required: true },
-    receive_comminication: { type: String, required: true },
-    document: { type: String, required: true },
+    template_title: { type: String, required: false },
+    logo: { type: String, required: false },
+    banner: { type: String, required: false },
+    form_type: { type: String, required: false },
+    form_link: { type: String, required: false },
+    receive_comminication: { type: String, required: false },
+    document: { type: String, required: false },
     html_content: { type: String, required: false },
+    date: { type: Date, default: Date.now } // Adds date with current timestamp as default
 });
 
 const Template = mongoose.model("Template", templateSchema);
